7661ec27277665cd07898473a25ad0d8d6c68c43
Oleg Lavrovsky <oleg@datalets.ch>
2023-02-03 14:07:39 +0100

    NLP chatbot

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -2,9 +2,14 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # Connect to our data (TODO: use an API-module)
-import read_csv from pandas
-Party = pd.read_csv('../data/Party.csv')
+import nlpcloud
+from pandas import read_csv
+Party = read_csv('../data/Party.csv')
 
+# Private tokens for our demo (TODO: store in environment!)
+API_KEY = "4329be765d81cb0c809270d9911cb3798c541fd5"
+TELEGRAM_KEY = "6146055236:AAG87qRlGXkXLJkVeDagYJcvlg9CyPZSqDE"
+    
 # Quick test routine
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
@@ -14,13 +19,26 @@ async def party(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     txt = Party.sample(n=1).get('PartyName').values[0]
     await update.message.reply_text(txt)
 
-# This is a private token for our bot (TODO: store in environment!)
-app = ApplicationBuilder().token("6146055236:AAG87qRlGXkXLJkVeDagYJcvlg9CyPZSqDE").build()
+# Say something smart
+async def parl(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    qry = ' '.join(context.args)
+    print(qry)
+    txt = nlpcli.chatbot(qry,
+        context="""Dies ist eine Diskussion über Parlamentswahlen basierend auf den Daten von parlament.ch"""
+    )['response']
+    await update.message.reply_text(txt)
+
+# Set up the app
+app = ApplicationBuilder().token(TELEGRAM_KEY).build()
+
+# Connect to NLP cloud
+nlpcli = nlpcloud.Client("finetuned-gpt-neox-20b", API_KEY, gpu=True)
 
 # Add the comamnds
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("party", party))
+app.add_handler(CommandHandler("parl", parl))
 
 # Start the loop
-app.run_polling()
 print("Bot operational. Press Ctrl-C to exit ..")
+app.run_polling()
