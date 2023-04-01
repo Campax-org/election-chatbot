343b43c3de6b4cea41cefbd465f123b14302eef7
stajilov <stazhilov@gmail.com>
2023-04-01 12:03:20 +0200

    swiss bert improved

--- swiss_bert_test.py
+++ swiss_bert_test.py
@@ -10,4 +10,4 @@ fill_mask = pipeline(model="ZurichNLP/swissbert")
 fill_mask.model.set_default_language("de_CH")
 
 
-fill_mask("<mask> für Gleichstellungspolitik in Bern")
+result = fill_mask("<mask> für Gleichstellungspolitik in Bern")
