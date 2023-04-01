84ff12f77c048b1ec8c47f3da36646034e48f5aa
stajilov <stazhilov@gmail.com>
2023-04-01 10:28:30 +0200

    added test q

--- swiss_bert_test.py
+++ swiss_bert_test.py
@@ -1,23 +1,13 @@
-import tensorflow as tf
-
-cifar = tf.keras.datasets.cifar100
-(x_train, y_train), (x_test, y_test) = cifar.load_data()
-model = tf.keras.applications.ResNet50(
-    include_top=True,
-    weights=None,
-    input_shape=(32, 32, 3),
-    classes=100,)
-
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
-model.fit(x_train, y_train, epochs=5, batch_size=64)
+# 
 
 
-# from transformers import pipeline
+from transformers import pipeline
 
 
-# fill_mask = pipeline(model="ZurichNLP/swissbert")
+fill_mask = pipeline(model="ZurichNLP/swissbert")
 
-# # fr_CH, #it_CH #rm_CH
-# fill_mask.model.set_default_language("de_CH")
-# fill_mask("Der schönste Kanton der Schweiz ist <mask>.")
+# fr_CH, #it_CH #rm_CH
+fill_mask.model.set_default_language("de_CH")
+
+
+fill_mask("<mask> für Gleichstellungspolitik in Bern")
