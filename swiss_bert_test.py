3f9818e2e2eb0cf24d4e24fde2603785ae8cb1bf
stajilov <stazhilov@gmail.com>
2023-04-01 11:34:25 +0200

    swiss bert added

--- swiss_bert_test.py
+++ swiss_bert_test.py
@@ -4,7 +4,7 @@
 from transformers import pipeline
 
 
-fill_mask = pipeline(model="ZurichNLP/swissbert")
+fill_mask = pipeline(model="swissbert")
 
 # fr_CH, #it_CH #rm_CH
 fill_mask.model.set_default_language("de_CH")
